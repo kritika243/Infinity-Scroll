547b4863a7afaa638cd56b90e3c4b892c6b65f37
Kritika <24kritikasharma24@gmail.com>
2021-05-06 22:11:54 +0530

    fetch request to api

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 const count = 10
 const apiKey = config.access_key
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// get photos from unsplash api
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    // catch error here
+  }
+}
+
+// on load
+getPhotos()
